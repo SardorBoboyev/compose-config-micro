cce24b9c7aa591cded089050e21914022c0ea81f
SardorBoboyev <boboyevsardor350@gmail.com>
2024-10-16 23:20:30 +0500

    Initial commit

---
+++ src/main/java/uz/sb/composeconfigmicro/ComposeConfigMicroApplication.java
@@ -0,0 +1,13 @@
+package uz.sb.composeconfigmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposeConfigMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComposeConfigMicroApplication.class, args);
+    }
+
+}
